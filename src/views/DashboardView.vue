2db13d9faa3621601e324a1ac58bd56005175fb0
CodeRacon <michael.buschmann.86@gmail.com>
2024-07-31 18:33:11 +0200

    feat: #6 integrated the new donutchart-component into the dashboard-component; configured the needed options and plugins like tooltiip & hover

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -5,7 +5,10 @@
 		</section>
 		<section class="center">
 			<div class="line-chart">
-				<revenue-chart :stockData="chartData" />
+				<revenue-chart :chartData="revenueChartData" />
+			</div>
+			<div class="ttm-chart">
+				<donut-chart :chartData="donutChartData" />
 			</div>
 		</section>
 		<section class="bottom"></section>
@@ -18,30 +21,32 @@ import {
 	fetchStockData,
 	prepareStockTileData,
 	prepareRevenueChartData,
+	calculateTTMRevenue,
 } from '@/services/stockService';
 import StockTileList from '@/components/StockTileList.vue';
 import RevenueChart from '@/components/RevenueChart.vue';
+import DonutChart from '@/components/DonutChart.vue';
 
 export default {
 	name: 'DashboardView',
 	components: {
 		StockTileList,
 		RevenueChart,
+		DonutChart,
 	},
 	setup() {
-		const stockData = ref({});
+		const rawStockData = ref(null);
 		const stocks = ref([]);
-		const chartData = ref(null);
-		const isDataLoaded = computed(
-			() => Object.keys(stockData.value).length > 0
-		);
+		const revenueChartData = ref(null);
+		const donutChartData = ref(null);
+		const isDataLoaded = computed(() => rawStockData.value !== null);
 
 		const loadStockData = async () => {
 			try {
-				const rawData = await fetchStockData();
-				stockData.value = rawData;
-				stocks.value = Object.values(prepareStockTileData(rawData));
-				chartData.value = prepareRevenueChartData(rawData);
+				rawStockData.value = await fetchStockData();
+				stocks.value = Object.values(prepareStockTileData(rawStockData.value));
+				revenueChartData.value = prepareRevenueChartData(rawStockData.value);
+				donutChartData.value = calculateTTMRevenue(rawStockData.value);
 			} catch (error) {
 				console.error('Error loading stock data:', error);
 			}
@@ -50,9 +55,9 @@ export default {
 		onMounted(loadStockData);
 
 		return {
-			stockData,
 			stocks,
-			chartData,
+			revenueChartData,
+			donutChartData,
 			isDataLoaded,
 		};
 	},
