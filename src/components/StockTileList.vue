517c4b58049e19fb6cd256535d0ad27b74c9d176
CodeRacon <michael.buschmann.86@gmail.com>
2024-08-01 21:06:00 +0200

    feat: #3 added logic to show an 'overflow-indicator' when content is overflowing on x-axis

--- src/components/StockTileList.vue
+++ src/components/StockTileList.vue
@@ -1,11 +1,12 @@
 <template>
-	<div class="stock-tile-list">
+	<div class="stock-tile-list" ref="tileListContainer">
 		<stock-tile v-for="stock in stocks" :key="stock.name" :stock="stock" />
 	</div>
 </template>
 
 <script>
 import StockTile from './StockTile.vue';
+import { ref, onMounted, onUnmounted } from 'vue';
 
 export default {
 	name: 'StockTileList',
@@ -15,6 +16,39 @@ export default {
 			type: Array,
 			required: true,
 		},
+	},
+
+	setup() {
+		const tileListContainer = ref(null);
+
+		const checkOverflow = () => {
+			if (tileListContainer.value) {
+				const container = tileListContainer.value;
+				const hasLeftOverflow = container.scrollLeft > 0;
+				const hasRightOverflow =
+					container.scrollLeft < container.scrollWidth - container.clientWidth;
+
+				container.classList.toggle('left-overflow', hasLeftOverflow);
+				container.classList.toggle('right-overflow', hasRightOverflow);
+			}
+		};
+
+		onMounted(() => {
+			window.addEventListener('resize', checkOverflow);
+			if (tileListContainer.value) {
+				tileListContainer.value.addEventListener('scroll', checkOverflow);
+			}
+			checkOverflow();
+		});
+
+		onUnmounted(() => {
+			window.removeEventListener('resize', checkOverflow);
+			if (tileListContainer.value) {
+				tileListContainer.value.removeEventListener('scroll', checkOverflow);
+			}
+		});
+
+		return { tileListContainer };
 	},
 };
 </script>
