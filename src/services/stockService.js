c7850b638785bc784bb5d7fb79cbcc9ed1c85af8
CodeRacon <michael.buschmann.86@gmail.com>
2024-08-03 11:22:33 +0200

    feat/doc: #10 created function to prepare gross margin data to be visualized in the chart; added seperate color-palette for grouped bar chart; added jsdoc

--- src/services/stockService.js
+++ src/services/stockService.js
@@ -1,9 +1,19 @@
 import axios from 'axios';
 
+/**
+ * The Google Sheets API ID and API key used to fetch stock data.
+ * @constant
+ * @type {string}
+ */
 const SHEET_ID = '1t1ik8tsW02LwyLhD7er6zPQyT1FAH5ELo0pppzf95EE';
-
 const API_KEY = 'AIzaSyDNRVNz-2--OLPjUTuntGFd6eUOkveBKYM';
 
+/**
+ * An array of stock symbols and their corresponding company names.
+ * This array is used to fetch and process stock data for the supported companies.
+ * @constant
+ * @type {Array<{ name: string, symbol: string }>}
+ */
 const stockSymbols = [
 	{ name: 'Apple', symbol: 'AAPL' },
 	{ name: 'Amazon', symbol: 'AMZN' },
@@ -14,6 +24,12 @@ const stockSymbols = [
 	{ name: 'Tesla', symbol: 'TSLA' },
 ];
 
+/**
+ * An object that maps stock symbols to the indices of relevant data in the stock data array.
+ * The indices correspond to the position of the data in the values array returned from the Google Sheets API.
+ * @constant
+ * @type {Object<string, { recordedQuarters: number, revenue: number, netIncome: number, grossMargin: number }>}
+ */
 const dataIndexes = {
 	AAPL: { recordedQuarters: 2, revenue: 8, netIncome: 35, grossMargin: 22 },
 	AMZN: { recordedQuarters: 2, revenue: 8, netIncome: 40, grossMargin: 14 },
@@ -24,6 +40,11 @@ const dataIndexes = {
 	TSLA: { recordedQuarters: 2, revenue: 12, netIncome: 43, grossMargin: 25 },
 };
 
+/**
+ * Fetches stock data from the Google Sheets API and returns an object containing the data for each stock symbol.
+ * @returns {Promise<Object<string, { values: string[] }>>} An object where the keys are stock symbols and the values are the corresponding stock data from the Google Sheets API.
+ * @throws {Error} If there is an error fetching the stock data.
+ */
 export const fetchStockData = async () => {
 	try {
 		const stockData = {};
@@ -39,6 +60,13 @@ export const fetchStockData = async () => {
 		throw error;
 	}
 };
+
+/**
+ * Prepares an object containing tile data for each stock symbol.
+ * The tile data includes the current revenue, quarter, absolute change, and relative change in revenue.
+ * @param {Object<string, { values: string[] }>} data - An object containing the stock data for each symbol.
+ * @returns {Object<string, { name: string, symbol: string, currentRevenue: string, quarter: string, absoluteChange: string, relativeChange: string }>} - An object containing the tile data for each stock symbol.
+ */
 export const prepareStockTileData = (data) => {
 	const tileData = {};
 	for (const stock of stockSymbols) {
@@ -49,6 +77,12 @@ export const prepareStockTileData = (data) => {
 	return tileData;
 };
 
+/**
+ * Prepares the data for a revenue chart, including the labels and datasets.
+ *
+ * @param {Object<string, { values: string[] }>} data - An object containing the stock data for each symbol.
+ * @returns {Object} An object containing the chart data, including labels and datasets.
+ */
 export const prepareRevenueChartData = (data) => {
 	const chartData = {
 		labels: [],
@@ -57,7 +91,6 @@ export const prepareRevenueChartData = (data) => {
 
 	const quarters = data[stockSymbols[0].symbol].values[2].slice(-13);
 	chartData.labels = quarters.map((quarter) => formatQuarter(quarter));
-	console.log(chartData);
 
 	stockSymbols.forEach((stock, index) => {
 		const companyData =
@@ -73,6 +106,13 @@ export const prepareRevenueChartData = (data) => {
 	return chartData;
 };
 
+/**
+ * Calculates the tile data for a given stock symbol, including the current revenue, quarter, absolute change, and relative change in revenue.
+ *
+ * @param {Object} data - The data object containing the stock information.
+ * @param {string} symbol - The stock symbol.
+ * @returns {Object} An object containing the tile data for the given stock symbol.
+ */
 const calculateTileData = (data, symbol) => {
 	const indexes = dataIndexes[symbol];
 	const revenueData = data.values[indexes.revenue];
@@ -91,6 +131,12 @@ const calculateTileData = (data, symbol) => {
 	};
 };
 
+/**
+ * Calculates the Trailing Twelve Months (TTM) revenue for a set of stock symbols.
+ *
+ * @param {Object} data - The data object containing the stock information.
+ * @returns {Object} An object containing the TTM revenue values and corresponding colors for each stock symbol.
+ */
 export const calculateTTMRevenue = (data) => {
 	const ttmRevenue = {};
 	const colors = [];
@@ -110,6 +156,12 @@ export const calculateTTMRevenue = (data) => {
 	return { ttmRevenue, colors };
 };
 
+/**
+ * Prepares the net income TTM (Trailing Twelve Months) data for a set of stock symbols.
+ *
+ * @param {Object} data - The data object containing the stock information.
+ * @returns {Object} An object containing the net income TTM data, including labels and datasets.
+ */
 export const prepareNetIncomeTTMData = (data) => {
 	const netIncomeTTM = {};
 
@@ -139,10 +191,109 @@ export const prepareNetIncomeTTMData = (data) => {
 	};
 };
 
+/**
+ * Prepares gross margin data for a set of stock symbols.
+ *
+ * @param {Object} data - The data object containing the stock information.
+ * @returns {Object} An object containing the gross margin data, including labels, datasets, and quarters.
+ */
+export const prepareGrossMarginData = (data) => {
+	const grossMarginData = {};
+
+	stockSymbols.forEach((stock) => {
+		const grossMarginValues =
+			data[stock.symbol].values[dataIndexes[stock.symbol].grossMargin];
+		const lastQuarterGrossMargin = parseFloat(
+			grossMarginValues[grossMarginValues.length - 1]
+		);
+		const lastQuarter =
+			data[stock.symbol].values[dataIndexes[stock.symbol].recordedQuarters][
+				grossMarginValues.length - 1
+			];
+		grossMarginData[stock.name] = {
+			value: lastQuarterGrossMargin,
+			quarter: lastQuarter,
+		};
+	});
+
+	const sortedData = Object.entries(grossMarginData)
+		.sort(([, a], [, b]) => b.value - a.value)
+		.reduce((r, [k, v]) => ({ ...r, [k]: v }), {});
+
+	return {
+		labels: Object.keys(sortedData),
+		datasets: [
+			{
+				data: Object.values(sortedData).map((item) => item.value),
+				backgroundColor: stockSymbols.map((_, index) => getColor(index)),
+			},
+		],
+		quarters: Object.values(sortedData).map((item) => item.quarter),
+	};
+};
+
+/**
+ * Prepares revenue growth data for a set of stock symbols.
+ *
+ * @param {Object} data - The data object containing the stock information.
+ * @returns {Object} An object containing the revenue growth data, including labels and datasets.
+ */
+export const prepareRevenueGrowthData = (data) => {
+	const revenueGrowthData = {};
+	const quarters = [];
+
+	stockSymbols.forEach((stock) => {
+		const revenueData =
+			data[stock.symbol].values[dataIndexes[stock.symbol].revenue];
+		const growthRates = [];
+
+		for (let i = revenueData.length - 1; i >= revenueData.length - 4; i--) {
+			const currentRevenue = parseCommaNumber(revenueData[i]);
+			const previousYearRevenue = parseCommaNumber(revenueData[i - 4]);
+			const growthRate =
+				((currentRevenue - previousYearRevenue) / previousYearRevenue) * 100;
+			growthRates.unshift(parseFloat(growthRate.toFixed(2)));
+
+			if (quarters.length < 4) {
+				quarters.unshift(
+					formatQuarter(
+						data[stock.symbol].values[
+							dataIndexes[stock.symbol].recordedQuarters
+						][i]
+					)
+				);
+			}
+		}
+
+		revenueGrowthData[stock.name] = growthRates;
+	});
+
+	return {
+		labels: stockSymbols.map((stock) => stock.name),
+		datasets: quarters.map((quarter, index) => ({
+			label: quarter,
+			data: Object.values(revenueGrowthData).map((rates) => rates[index]),
+			backgroundColor: compareColors(index),
+		})),
+	};
+};
+
+/**
+ * Parses a string value with a comma as the decimal separator and returns a floating-point number.
+ *
+ * @param {string} value - The string value to parse.
+ * @returns {number} The parsed floating-point number.
+ */
 const parseCommaNumber = (value) => {
 	return parseFloat(value.replace(',', '.'));
 };
 
+/**
+ * Formats a quarter string in the format "Q#" followed by the year.
+ *
+ * @param {string} quarter - The quarter string to format, in the format "Q#" or "YYYY Q#".
+ * @returns {string} The formatted quarter string in the format "Q# YYYY".
+ */
 export const formatQuarter = (quarter) => {
 	const parts = quarter.replace('-', ' ').split(/\s+/);
 	let q, year;
@@ -164,6 +315,13 @@ export const formatQuarter = (quarter) => {
 	return `${q} ${year}`;
 };
 
+/**
+ * Generates a color based on the provided index.
+ * The colors are chosen from a predefined palette and cycled through as the index increases.
+ *
+ * @param {number} index - The index to use for selecting the color.
+ * @returns {string} - The hex color code.
+ */
 const getColor = (index) => {
 	const colors = [
 		'#ff7d7d',
@@ -174,5 +332,17 @@ const getColor = (index) => {
 		'#7dd0ff',
 		'#7dffef',
 	];
+	return colors[index % colors.length];
+};
+
+/**
+ * Generates a color based on the provided index.
+ * The colors are chosen from a predefined palette and cycled through as the index increases.
+ *
+ * @param {number} index - The index to use for selecting the color.
+ * @returns {string} - The hex color code.
+ */
+const compareColors = (index) => {
+	const colors = ['#5390d9', '#4ea8de', '#48bfe3', '#56cfe1'];
 	return colors[index % colors.length];
 };
