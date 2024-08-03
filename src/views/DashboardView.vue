83deda13a1c8e984dc27269e3a7b53ca44eede79
CodeRacon <michael.buschmann.86@gmail.com>
2024-08-03 11:43:15 +0200

    refactor/feat/doc: #2 re-aranged template-structure due to responsive issues; implemented gross margin-chart + rev.growth-chart; added logic to emit 'data-loaded' event to hide spinner; added jsdoc

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,7 +1,8 @@
 <template>
 	<div class="dashboard" v-if="isDataLoaded">
 		<div class="heading">
-			<h1>The M7 Stock Earning Dashboard</h1>
+			<img src="../assets/logo.png" alt="logo" />
+			<h1>Stock Earning Dashboard</h1>
 		</div>
 		<section class="top">
 			<stock-tile-list :stocks="stocks" />
@@ -13,19 +14,24 @@
 			<div class="ttm-chart">
 				<donut-chart :chartData="donutChartData" />
 			</div>
-		</section>
-		<section class="bottom">
-			<div class="bar-charts">
-				<div class="netincome-bar-chart-1">
-					<net-income-chart :chartData="netIncomeChartData" />
-				</div>
-				<div class="netincome-bar-chart-2">
-					<net-income-chart :chartData="netIncomeChartData" />
-				</div>
+
+			<div class="glossary-m">
+				<glossary-component />
 			</div>
 
-			<div class="netincome-bar-chart-3">
+			<div class="revenuegrowth-bar-chart">
+				<revenue-growth-chart :chartData="revenueGrowthChartData" />
+			</div>
+
+			<div class="netincome-bar-chart">
 				<net-income-chart :chartData="netIncomeChartData" />
+			</div>
+			<div class="grossmargin-bar-chart">
+				<gross-margin-chart :chartData="grossMarginChartData" />
+			</div>
+
+			<div class="glossary-s-l">
+				<glossary-component />
 			</div>
 		</section>
 	</div>
@@ -39,11 +45,16 @@ import {
 	prepareRevenueChartData,
 	calculateTTMRevenue,
 	prepareNetIncomeTTMData,
+	prepareGrossMarginData,
+	prepareRevenueGrowthData,
 } from '@/services/stockService';
 import StockTileList from '@/components/StockTileList.vue';
 import RevenueChart from '@/components/RevenueChart.vue';
 import DonutChart from '@/components/DonutChart.vue';
 import NetIncomeChart from '@/components/NetIncomeChart.vue';
+import GrossMarginChart from '@/components/GrossMarginChart.vue';
+import RevenueGrowthChart from '@/components/RevenueGrowthChart.vue';
+import GlossaryComponent from '@/components/GlossaryComponent.vue';
 
 export default {
 	name: 'DashboardView',
@@ -52,13 +63,29 @@ export default {
 		RevenueChart,
 		DonutChart,
 		NetIncomeChart,
+		GrossMarginChart,
+		RevenueGrowthChart,
+		GlossaryComponent,
 	},
-	setup() {
+
+	/**
+	 * The `setup` function is a Vue.js composition API function that is called when the component is created. It is responsible for initializing the component's state and handling any side effects.
+	 *
+	 * The `setup` function in this component does the following:
+	 * - Initializes several reactive references to hold the raw stock data, the prepared stock tile data, and the data for various charts.
+	 * - Defines a `loadStockData` function that fetches the stock data, prepares it, and updates the component's state accordingly.
+	 * - Calls the `loadStockData` function when the component is mounted using the `onMounted` hook.
+	 * - Defines a computed property `isDataLoaded` that indicates whether the stock data has been loaded.
+	 * - Returns an object containing the component's state and the `isDataLoaded` computed property.
+	 */
+	setup(props, { emit }) {
 		const rawStockData = ref(null);
 		const stocks = ref([]);
 		const revenueChartData = ref(null);
 		const donutChartData = ref(null);
 		const netIncomeChartData = ref(null);
+		const grossMarginChartData = ref(null);
+		const revenueGrowthChartData = ref(null);
 
 		const isDataLoaded = computed(() => rawStockData.value !== null);
 
@@ -69,6 +96,11 @@ export default {
 				revenueChartData.value = prepareRevenueChartData(rawStockData.value);
 				donutChartData.value = calculateTTMRevenue(rawStockData.value);
 				netIncomeChartData.value = prepareNetIncomeTTMData(rawStockData.value);
+				grossMarginChartData.value = prepareGrossMarginData(rawStockData.value);
+				revenueGrowthChartData.value = prepareRevenueGrowthData(
+					rawStockData.value
+				);
+				emit('data-loaded');
 			} catch (error) {
 				console.error('Error loading stock data:', error);
 			}
@@ -81,6 +113,8 @@ export default {
 			revenueChartData,
 			donutChartData,
 			netIncomeChartData,
+			grossMarginChartData,
+			revenueGrowthChartData,
 
 			isDataLoaded,
 		};
@@ -88,6 +122,6 @@ export default {
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 @import '../styles/dashboard.scss';
 </style>
